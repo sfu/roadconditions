54b48e62d95b82486ea270f6298e07f2b93db63e
Graham Ballantyne <grahamb@sfu.ca>
2012-03-30 13:36:37 -0700

    use logger

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express')
   , routes = require('./routes');
 
 var app = module.exports = express.createServer();
+var app = module.exports = express.createServer(express.logger());
 
 // Configuration
 
