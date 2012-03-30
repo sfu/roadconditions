c1d85034a4c26941dfe65e13c2729207ade05b06
Graham Ballantyne <grahamb@sfu.ca>
2012-03-30 13:34:09 -0700

    undo commit

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.configure(function(){
 });
 
 app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
