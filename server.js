329bd9f87ea59dbc2a70c10713b7ef3199d45978
Graham Ballantyne <grahamb@sfu.ca>
2012-05-22 22:02:59 -0700

    change port env

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ var writeConditions = function(data) {
 
 // Configuration
 
-app.listen(process.env.PORT || 3001);
+app.listen(process.env.app_port || 3001);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 
