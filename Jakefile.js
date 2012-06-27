0c74d09d66200166fb30bf0da570f7770be5c48e
Graham Ballantyne <grahamb@sfu.ca>
2012-06-27 16:26:41 -0700

    lots of jakefile stuff
    whatever

--- Jakefile.js
+++ Jakefile.js
@@ -1,5 +1,52 @@
-var spawn = require('child_process').spawn;
+/*global desc, task, complete*/
 
+var spawn = require('child_process').spawn
+,   fs = require('fs')
+,   fileEnc = 'utf-8';
+
+var uglify = function(file) {
+    var uglyfyJS = require('uglify-js')
+    ,   path = require('path')
+    ,   jsp = uglyfyJS.parser
+    ,   pro = uglyfyJS.uglify
+    ,   ast = jsp.parse(fs.readFileSync(file, fileEnc))
+    ,   dirname = path.dirname(file)
+    ,   filename = path.basename(file, '.js')
+    ,   distFile = path.join(dirname, filename + '-min.js');
+
+    ast = pro.ast_mangle(ast);
+    ast = pro.ast_squeeze(ast);
+
+    fs.writeFileSync(distFile, pro.gen_code(ast), fileEnc);
+    var msg = ' + minified ' + file + ' to ' + distFile;
+    console.log(msg.green);
+};
+
+var jslintFile = function(file) {
+    var jshint = require('jshint')
+    ,   buf = fs.readFileSync(file, fileEnc);
+
+    buf = buf.replace(/^\uFEFF/, '');
+    jshint.JSHINT(buf);
+
+    return jshint.JSHINT.errors.lengh ? [1, jshint.JSHINT.errors] : [0, null];
+};
+
+
+
+var clientJsFiles = [
+    __dirname + '/public/js/admin.js',
+    __dirname + '/public/js/menus.js',
+    __dirname + '/public/js/refreshcameras.js'
+];
+
+var serverJsFiles = [ __dirname + '/server.js' ];
+
+var cssFiles = [
+    __dirname + '/public/css/admin-dispatcher.css',
+    __dirname + '/public/css/base.css',
+    __dirname + '/public/css/conditions.css'
+];
 
 desc('Install modules from npm');
 task('install-npm-deps', [], function() {
@@ -7,7 +54,7 @@ task('install-npm-deps', [], function() {
 
     console.log('    Executing command:\n    $ npm install\n'.grey);
 
-    npm = spawn('npm', ['install']);
+    var npm = spawn('npm', ['install']);
 
     npm.stdout.on('data', function (data) {
         process.stdout.write(('    ' + data).grey);
@@ -25,69 +72,94 @@ task('install-npm-deps', [], function() {
             throw new Error('npm exited with error code ' + code);
         }
     });
-
 }, true);
 
 desc('jshint node files');
 task('jshint-node', [], function() {
-
-    console.log('\n > Attempting to run jshint on server files'.blue);
-
-    jshint = spawn('jshint', ['--config', '.jshintrc-server', 'server.js']);
-
-    jshint.stdout.on('data', function (data) {
-        process.stdout.write(('    ' + data).grey);
-    });
-
-    jshint.stderr.on('data', function (data) {
-        process.stdout.write(('    ' + data).grey);
-    });
-
-    jshint.on('exit', function(code) {
-        if (code === 0) {
-            console.log(' + jshinted node files successfully'.green);
-            complete();
+    console.log('\n > Attempting to run jshint on server nodejs files'.blue);
+    var count = 0;
+    serverJsFiles.forEach(function(file) {
+        var result = jslintFile(file);
+        if (result[0] === 0) {
+            var msg = ' +  ' + file + ' is ok';
+            console.log(msg.green);
+            if (++count === serverJsFiles.length) {
+                complete();
+            }
         } else {
-            throw new Error('jshint exited with error code ' + code);
+            console.log(result[1]);
+            throw new Error('jshint found problems with ' + file);
         }
     });
-
 });
 
 desc('jsint client-side js files');
 task('jshint-client', [], function() {
-    console.log('\n > Attempting to run jshint on server files'.blue);
-
-    jshint = spawn('jshint', ['--config', '.jshintrc-cliet', 'public/js/*.js']);
-
-    jshint.stdout.on('data', function (data) {
-        process.stdout.write(('    ' + data).grey);
-    });
-
-    jshint.stderr.on('data', function (data) {
-        process.stdout.write(('    ' + data).grey);
-    });
-
-    jshint.on('exit', function(code) {
-        if (code === 0) {
-            console.log(' + jshinted node files successfully'.green);
-            complete();
+    console.log('\n > Attempting to run jshint on client files'.blue);
+    var count = 0;
+    clientJsFiles.forEach(function(file) {
+        var result = jslintFile(file);
+        if (result[0] === 0) {
+            var msg = ' +  ' + file + ' is ok';
+            console.log(msg.green);
+            if (++count === clientJsFiles.length) {
+                complete();
+            }
         } else {
-            throw new Error('jshint exited with error code ' + code);
+            console.log(result[1]);
+            throw new Error('jshint found problems with ' + file);
         }
-    });
+        });
+});
 
+desc('minify client-side js files');
+task('minify-client-js', ['jshint-client'], function() {
+    console.log('\n > Attempting to minify client js files'.blue);
+    var distDir = 'public/js';
+
+    clientJsFiles.forEach(function(file) {
+        uglify(file, distDir);
+    });
+    complete();
+});
+
+desc('minify css files');
+task('minify-css', [], function() {
+    var less = require('less')
+    ,   parser = new less.Parser()
+    ,   path = require('path')
+    ,   count = 0;
+
+    cssFiles.forEach(function(file) {
+        var css = fs.readFileSync(file, fileEnc)
+        ,   dirname = path.dirname(file)
+        ,   filename = path.basename(file, '.css')
+        ,   distFile = path.join(dirname, filename + '-min.css');
+
+        parser.parse(css, function(e, tree) {
+            if (e) {
+                return [1, e];
+            } else {
+                fs.writeFileSync(distFile, tree.toCSS({compress: true}), fileEnc);
+                var msg = ' + minified ' + file + ' to ' + distFile;
+                console.log(msg.green);
+                if (++count === cssFiles.length) {
+                    complete();
+                }
+            }
+        });
+    });
 });
 
 
 desc('default task');
-task('default', ['install-npm-deps'], function() {
-    console.log('\n\n > Attempting to put the site live\n'.blue);
+task('default', [], function() {
+    console.log('\n\n > Nothing to do here. How about running an actual task?\n'.blue);
 });
 
 
 
-function stylize(str, style) {
+var stylize = function (str, style) {
   var styles = {
   //styles
   'bold'      : [1,  22], 'italic'    : [3,  23],
@@ -101,7 +173,7 @@ function stylize(str, style) {
   'red'       : [31, 39],'yellow'    : [33, 39]
   };
   return '\033[' + styles[style][0] + 'm' + str + '\033[' + styles[style][1] + 'm';
-}
+};
 
 ['bold', 'underline', 'italic',
     'inverse', 'grey', 'yellow',
@@ -113,3 +185,4 @@ function stylize(str, style) {
   });
 
 });
+
