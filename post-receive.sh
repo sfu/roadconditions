ffbd8ab17d41ae7f766916a6edf3c3e7a834fb6a
Graham Ballantyne <grahamb@sfu.ca>
2012-10-01 17:01:15 -0700

    handle different branches in post-receive hook

--- post-receive.sh
+++ post-receive.sh
@@ -8,13 +8,21 @@ if [ -d "$TMPDIR" ]; then
 fi
 
 mkdir $TMPDIR
-echo "checking out files to $TMPDIR"
-git --work-tree=$TMPDIR checkout -f master
+if ! [ -t 0 ]; then
+  read -a ref
+fi
+
+IFS='/' read -ra REF <<< "${ref[2]}"
+
+BRANCH="${REF[2]}"
+
+echo "checking out branch $BRANCH to $TMPDIR"
+git --work-tree=$TMPDIR checkout -f $BRANCH
 cd $TMPDIR
+/sbin/service roadconditions stop
 jake install-npm-deps
 jake prepfiles
 jake createdir
-service roadconditions stop
 jake symlink
 printf $SHA > $TMPDIR/gitsha
 jake install
