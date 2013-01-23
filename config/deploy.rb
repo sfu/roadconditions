3f03aaef5a0f0beb74ed111ca00c938b38064706
Graham Ballantyne <grahamb@sfu.ca>
2013-01-23 11:25:41 -0800

    call init.d script over a serial ssh connection to each server
    workaround cap terminating the process

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,8 +49,9 @@ namespace :deploy do
     end
 
     task :restart do
-        run "/sbin/service roadconditions stop"
-        run "/sbin/service roadconditions start"
+        find_servers_for_task(current_task).each do |server|
+            run_locally "ssh #{user}@#{server} /etc/init.d/roadconditions restart"
+        end
     end
 
 end
