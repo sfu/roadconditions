2163cacaa814350c66960e38bfd842158fb96879
Graham Ballantyne <grahamb@sfu.ca>
2013-10-25 15:40:48 -0700

    remove config_file from stage files

--- config/deploy/production_2.rb
+++ config/deploy/production_2.rb
@@ -1,4 +1,3 @@
 role :app, "nodejs2.tier2.sfu.ca"
 set :node_env, "production"
 set :branch, "master"
-set :config_file = "config.production.json"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,2 @@
 role :app, "node-stage.its.sfu.ca"
 set :node_env, "production"
-set :config_file = "config.staging.json"
