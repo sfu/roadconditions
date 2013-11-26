57f16b2430929ca954c71423445354040ee97abf
Graham Ballantyne <grahamb@sfu.ca>
2013-11-26 14:55:13 -0800

    add Array#forEach pollyfill

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,6 +2,18 @@
 
 window.exports = {};
 
+if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function (fn, scope) {
+        'use strict';
+        var i, len;
+        for (i = 0, len = this.length; i < len; ++i) {
+            if (i in this) {
+                fn.call(scope, this[i], i, this);
+            }
+        }
+    };
+}
+
 var app = (function(Spine, $, exports, data) {
 
     var stripwhitespace = function() {
