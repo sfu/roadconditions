cc1e6a0631ff5de3bdbc433da35b1f2cbb58f745
Graham Ballantyne <grahamb@sfu.ca>
2017-09-27 16:29:30 -0700

    Remove Array.prototype.forEach polyfill

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,18 +1,5 @@
 /*global Spine, EJS, tinyMCE, moment*/
 
-window.exports = {};
-
-if (!Array.prototype.forEach) {
-    Array.prototype.forEach = function (fn, scope) {
-        'use strict';
-        var i, len;
-        for (i = 0, len = this.length; i < len; ++i) {
-            if (i in this) {
-                fn.call(scope, this[i], i, this);
-            }
-        }
-    };
-}
 
 var app = (function(Spine, $, exports, data) {
 
