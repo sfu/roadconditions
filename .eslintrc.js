695f3f9bf9b87cf43698d6dc129382cc2a141dd2
Graham Ballantyne <grahamb@sfu.ca>
2017-09-27 16:04:04 -0700

    Add eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {},
+  extends: ['eslint:recommended'],
+  globals: {},
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  rules: {
+    'no-console': 0
+  },
+  plugins: [],
+  settings: {}
+}
