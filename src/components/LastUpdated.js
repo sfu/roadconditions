08986bf34ef34f761aea4b052ddd48cd5e3f0fe8
Graham Ballantyne <grahamb@sfu.ca>
2017-10-18 09:55:08 -0700

    Move LastUpdated to components directory

--- src/components/LastUpdated.js
+++ src/components/LastUpdated.js
@@ -2,19 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import format from 'date-fns/format'
 
-const LastUpdated = ({ lastUpdated }) => {
+const LastUpdated = ({ at }) => {
   return (
     <section id="lastupdated" className="main update">
-      <p>
-        Last updated{' '}
-        {format(lastUpdated, '[at] h:mm a [on] dddd, MMMM DD, YYYY')}
-      </p>
+      <p>Last updated {format(at, '[at] h:mm a [on] dddd, MMMM DD, YYYY')}</p>
     </section>
   )
 }
 
 LastUpdated.propTypes = {
-  lastUpdated: PropTypes.number
+  at: PropTypes.number
 }
 
 export default LastUpdated
