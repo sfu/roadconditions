b765693a57938352f77a8ecd1329fe0b8486378a
Graham Ballantyne <grahamb@sfu.ca>
2017-10-18 09:56:33 -0700

    <App> initial commit

---
+++ src/components/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import deepmerge from 'deepmerge'
+
+import LastUpdated from 'components/LastUpdated'
+import Campus from 'components/Campus'
+import RoadStatus from 'components/RoadStatus'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: {},
+      fetching: true,
+      dirty: false
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  componentDidMount() {
+    axios
+      .get('/api/3/current')
+      .then(response => {
+        this.setState({
+          fetching: false,
+          data: response.data
+        })
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
+  handleChange(update) {
+    const newState = deepmerge(this.state, update)
+    newState.dirty = true
+    this.setState(newState)
+  }
+
+  render() {
+    if (this.state.fetching) {
+      return <p>Loading...</p>
+    } else {
+      const { data } = this.state
+      return (
+        <div>
+          <LastUpdated at={data.lastUpdated} />
+          <form>
+            <Campus campus="burnaby" data={data.campuses.burnaby}>
+              <RoadStatus changeHandler={this.handleChange} />
+            </Campus>
+            <Campus campus="vancouver" data={data.campuses.vancouver} />
+            <Campus campus="surrey" data={data.campuses.surrey} />
+          </form>
+        </div>
+      )
+    }
+  }
+}
