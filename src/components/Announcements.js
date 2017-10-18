e3956d625e58be87aafa2d221d60d21e2fd33bbe
Graham Ballantyne <grahamb@sfu.ca>
2017-10-18 11:57:28 -0700

    Add react-tinymce init options

--- src/components/Announcements.js
+++ src/components/Announcements.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import TinyMCE from 'react-tinymce/dist/react-tinymce.js'
+import TinyMCE from 'react-tinymce'
 
 const Announcements = ({ data }) => {
   const { announcements } = data
   return (
     <fieldset>
       <label>Announcements</label>
-      <TinyMCE content={announcements} />
+      <TinyMCE
+        content={announcements}
+        config={{
+          height: 350,
+          menubar: false,
+          statusbar: false,
+          plugins: 'paste, hr, contextmenu, link',
+          paste_remove_spans: true,
+          paste_strip_class_attributes: 'all',
+          paste_word_valid_elements: 'b,strong,i,em',
+          toolbar: 'bold italic | hr | link unlink | cut copy paste',
+          contextmenu: 'bold italic | link | cut copy paste'
+        }}
+      />
     </fieldset>
   )
 }
