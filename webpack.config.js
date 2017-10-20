b77cd37301103650a5a8ebb07dd217df9c88cb98
Graham Ballantyne <grahamb@sfu.ca>
2017-10-20 10:46:59 -0700

    Refactor webpack config to function, add NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,33 @@
 const { resolve } = require('path')
+const webpack = require('webpack')
 
-module.exports = {
-  entry: resolve('./src'),
-  output: {
-    path: resolve('./public/js/admin'),
-    filename: 'admin.bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      }
+module.exports = (env = {}) => {
+  return {
+    entry: resolve('./src'),
+    output: {
+      path: resolve('./public/js/admin'),
+      filename: 'admin.bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx'],
+      modules: [resolve(__dirname, 'src'), resolve(__dirname, 'node_modules')]
+    },
+    devtool: 'eval',
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: env.prod ? '"production"' : '"development"'
+        }
+      })
     ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-    modules: [resolve(__dirname, 'src'), resolve(__dirname, 'node_modules')]
-  },
-  devtool: 'eval'
+  }
 }
